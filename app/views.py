6cfceed0ada52a6131f2fba2526eddfdcf932700
Chris Sim <imchrissim@gmail.com>
2014-02-15 23:52:03 -0800

    added some commnets

--- app/views.py
+++ app/views.py
@@ -8,9 +8,12 @@ def index():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        return "password peeked!"
+    	return "Please check back later :D"
+        # Authenticate user (limit to 3 tries, then lock for 1hr, redirect to forgot password page)
+        # If first time using app, redirect to "what are you looking for?" page
+        # Display main deck, what makes mroe sense? pins on map showing what people are selling? or "trends" or "hot items" or "similar stuff"?
 
-@main.route('/test/')
+@main.route('/main/')
 def test():
-    return "fuck this shit"
+    return "Meh"
 
